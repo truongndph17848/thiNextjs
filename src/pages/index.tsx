1b1b9c5680897ec7a507461fe923af77967e6931
truongph17848 <truongndph17848@fpt.edu.vn>
2022-07-25 21:21:11 +0700

    update

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const index = () => {
+  return (
+
+        <div>
+            <h1 className='text-2xl text-center font-bold'> HOME </h1>
+        </div>
+  )
+}
+
+export default index
